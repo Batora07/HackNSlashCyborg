d405e112028b50ea3d2c244437020d11eca8741a
Marc Portet <marcsuitgundamseed@hotmail.fr>
2017-07-22 12:08:28 +0200

    startup project

---
+++ README.md
@@ -0,0 +1,11 @@
+# HackNSlashCyborg
+A hack' n slash game made in C++ with the SDL2 library.
+(To make the project works, you need to have the sdl2 library located at C:\sdl2)
+Made thanks to the assets and courses from Matthew Carr (https://twitter.com//2HitMatt)
+[https://www.udemy.com/hack-n-slash-sdl2/learn/v4/overview][1]
+
+Credit:
+Course programming, curriculum design, and lectures: Matthew Carr
+
+Project made with C++11 and Visual Studio 2015.
+The executable file will be included in the final commit.
---
+++ cyborg_battle/cyborg_battle/main.cpp
@@ -0,0 +1,75 @@
+#include "cleanup.h"
+#include "res_path.h"
+#include "drawing_functions.h"
+#include "SDL_mixer.h"
+
+int main(int argc, char **argv) {
+
+	// setup SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		cout << "error initializing SDL" << endl;
+		return 1;
+	}
+
+	// setup window
+	SDL_Window *window = SDL_CreateWindow("Cyborg Battle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 648, 352, SDL_WINDOW_SHOWN);
+	if (window == nullptr) {
+		SDL_Quit();
+		cout << "window error" << endl;
+		return 1;
+	}
+
+	// setup renderer
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr) {
+		cleanup(window);
+		SDL_Quit();
+		cout << "renderer error" << endl;
+		return 1;
+	}
+
+	// This is the size to draw things at, before we scale it to the screen size dimensions mentionned in createwindow
+	SDL_RenderSetLogicalSize(renderer, 640, 352);
+
+	// initialize sdl_image
+	if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
+		SDL_Quit();
+		cout << "sdl image did not initialize" << endl;
+		return 1;
+	}
+
+	//Initialize the text font
+	if (TTF_Init() != 0) {
+		SDL_Quit();
+		cout << "sdl ttf did not initialize" << endl;
+		return 1;
+	}
+
+	// initialize sdl mixer
+	if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1) {
+		SDL_Quit();
+		cout << "mixer did not initialize" << endl;
+		return 1;
+	}
+
+	// load up a texture to drawn
+	string resPath = getResourcePath();
+	SDL_Texture *texture = loadTexture(resPath + "map.png", renderer);
+
+	// run game for 5000 ticks (5000ms)
+	while (SDL_GetTicks() < 5000) {
+		// clear the screen
+		SDL_RenderClear(renderer);
+		// draw what we want to the screen
+		renderTexture(texture, renderer, 0, 0);
+		// show image we've been rendering
+		SDL_RenderPresent(renderer);
+	}
+
+	cleanup(renderer);
+	cleanup(window);
+	cleanup(texture);
+
+	SDL_Quit();
+	return 0;
+}
