3744a997d462287347f2bf6ceaad107a64cd678e
Marc Portet <marcsuitgundamseed@hotmail.fr>
2019-01-17 11:57:39 +0100

    Implement the swept AABB collision check method

--- cyborg_battle/cyborg_battle/entity.cpp
+++ cyborg_battle/cyborg_battle/entity.cpp
@@ -94,11 +94,12 @@ void Entity::updateCollisions() {
 		//list of potential collisions
 		list<Entity*> collisions;
 
+		// BROAD PHASE
 		//list<Entity*>::iterator entity = ...
 		for (auto entity = Entity::entities.begin(); entity != Entity::entities.end(); entity++) {
 			//if we collide with this entity with our currently unioned collisionbox, add to the list
 			if ((*entity)->active
-				&& (*entity)->type != this->type
+				//&& (*entity)->type != this->type  // add this line back if things of same type can walk through each other
 				&& (*entity)->solid
 				&& Entity::checkCollision(collisionBox, (*entity)->collisionBox)) {
 
@@ -110,6 +111,66 @@ void Entity::updateCollisions() {
 		//if we have a list of potential entities we may hit, then lets check them properly to do collision resolution
 		if (collisions.size() > 0) {
 			updateCollisionBox();
+
+			/** SWEPT AABB METHOD OF COLLISION **/
+
+			float collisionTime = 1; // 1 means no collisions, anything less, e.g 0.234, means we collided part of the way of our movement
+			float normalX, normalY;  // to help work out which side we crash into stuff
+
+			// our collisionBox before we try to move
+			SDL_Rect startingCollisionBox = lastCollisionBox;
+
+			//loop through the entities that are in our short list from broadphase;
+			for (auto entity = collisions.begin(); entity != collisions.end(); entity++) {
+				// temporary variables for normal x and y and also temporary collisionTime
+				float tmpNormalX, tmpNormalY;
+				float tmpCollisionTime = SweptAABB(startingCollisionBox, totalXMove, totalYMove, (*entity)->collisionBox, tmpNormalX, tmpNormalY);
+
+				// if this tmpCollisionTime is less than last collisionTime, use it instead
+				if (tmpCollisionTime < collisionTime) {
+					collisionTime = tmpCollisionTime;
+					normalX = tmpNormalX;
+					normalY = tmpNormalY;
+				}
+			}
+
+			//if there was a collision, let's slide off of it
+			if (collisionTime < 1.0f) {
+				// if we die on solids, run the crash function
+				if (dieOnSolids) {
+					crashOntoSolid();
+				}
+
+				// move our collisionBox position up to where we collided
+				startingCollisionBox.x += totalXMove * collisionTime;
+				startingCollisionBox.y += totalYMove * collisionTime;
+
+				// how much move time was remaining
+				float remainingTime = 1.0f - collisionTime;
+
+				//update entities x and y to where we bumped into other entity
+				x = startingCollisionBox.x + startingCollisionBox.w / 2;
+				y = startingCollisionBox.y - collisionBoxYOffset;
+
+				// COLLISION RESPONSE : SLIDE
+				// work out dotProduct using remainingTime
+				float dotProd = (totalXMove * normalY + totalYMove * normalX) * remainingTime;
+				totalXMove = dotProd * normalY;
+				totalYMove = dotProd * normalX;
+				x += totalXMove;
+				y += totalYMove;
+
+				// store collisionBox at this point
+				lastCollisionBox = startingCollisionBox;
+				//update our entites main collisionBox
+				updateCollisionBox();
+
+				//BECAUSE OUR SLIDING MAY HAVE BUMPED INTO OTHER OBJECTS, BETTER RUN THE FUNCTION AGAIN
+				updateCollisions();
+			}
+
+			/** MULTISAMPLE COLLISION METHOD **/
+			/*
 
 			//multisample check for collisions from where we started to where we are planning to go to
 
@@ -184,12 +245,115 @@ void Entity::updateCollisions() {
 				y = sampleBox.y - collisionBoxYOffset;
 			}
 
-			updateCollisionBox();
+			updateCollisionBox();*/
 		}
 	}
 }
 
 //HELP FUNCTIONS
+
+// return type : gives 0-1 depending on where collision is. 1 means no collisions, 0 means collide immediately and 0.5 is half way etc...
+//params : movingBox is entity being checked
+//         vx and vy are the velocities our moving box is moving at
+//         otherBox is some other entities collision box we may collide with
+//         normalX and normalY let us know which side of otherBox we collided with. These are pass by reference
+float Entity::SweptAABB(SDL_Rect movingBox, float vx, float vy, SDL_Rect otherBox, float &normalX, float &normalY) {
+	float xInvEntry, yInvEntry;  // entry points
+	float xInvExit, yInvExit;    // exit points
+
+	//find the distance between the objects on the near and far sides for both x and y
+	if (vx > 0.0f) {
+		// box moving to the right
+		// distance of the left hand side of otherBox to the right hand side of the moving box
+		xInvEntry = otherBox.x - (movingBox.x + movingBox.w);
+		// distance between the right hand side of otherBox to the left hand side of the moving box
+		xInvExit = (otherBox.x + otherBox.w) - movingBox.x;
+	}
+	else {
+		// box moving to the left
+		xInvEntry = (otherBox.x + otherBox.w) - movingBox.x;
+		xInvExit = otherBox.x - (movingBox.x + movingBox.w);
+	}
+
+	if (vy > 0.0f) {
+		// box moving to the top (to the bottom if y axis points down)
+		// distance of the left hand side of otherBox to the right hand side of the moving box
+		yInvEntry = otherBox.y - (movingBox.y + movingBox.h);
+		// distance between the right hand side of otherBox to the left hand side of the moving box
+		yInvExit = (otherBox.y + otherBox.h) - movingBox.y;
+	}
+	else {
+		// box moving to the bottom (to top if y axis points up)
+		yInvEntry = (otherBox.y + otherBox.h) - movingBox.y;
+		yInvExit = otherBox.y - (movingBox.y + movingBox.h);
+	}
+
+	//find time of collision and time of leaving for each axis (if statement is to prevent dividing by zero)
+	float xEntry, yEntry;
+	float xExit, yExit;
+
+	if (vx == 0.0f) {
+		xEntry = -std::numeric_limits<float>::infinity();
+		xExit = std::numeric_limits<float>::infinity();
+	}
+	else {
+		xEntry = xInvEntry / vx;  // if vx was zero we would have divided by 0 :p
+		xExit = xInvExit / vx;
+	}
+
+	if (vy == 0.0f) {
+		yEntry = -std::numeric_limits<float>::infinity();
+		yExit = std::numeric_limits<float>::infinity();
+	}
+	else {
+		yEntry = yInvEntry / vy;  
+		yExit = yInvExit / vy;
+	}
+
+	// find the earliest / latest time of collision
+	float entryTime = std::max(xEntry, yEntry);
+	float exitTime = std::min(xExit, yExit);
+
+	// if there was NO collision
+	if (entryTime > exitTime || xEntry < 0.0f && yEntry < 0.0f || xEntry > 1.0f || yEntry > 1.0f) {
+		normalX = 0.0f; // no x normal
+		normalY = 0.0f; // no y normal
+		return 1.0f; // 1 means moved to full distance, no collisions
+	}
+	else {
+		// there was a collision
+
+		// we should work out which sides/normals of the otherBox we collided with.
+		// assume we hit otherBox on the x Axis
+		if (xEntry > yEntry) {
+			
+			if (xInvEntry < 0.0f) {
+				normalX = 1; // hit left hand side
+				normalY = 0; // not hit top or bottom of otherBox
+			}
+			else {
+				normalX = -1; // hit right hand side
+				normalY = 0; // not hit top or bottom of otherBox
+			}
+		}
+		else {
+			// assume we hit otherBox on y axis
+			if (yInvEntry < 0.0f) {
+				normalX = 0;
+				normalY = 1;
+			}
+			else {
+				normalX = 0;
+				normalY = -1;
+			}
+		}
+
+		// return the time of collision (0-1 eg : 0.234234)
+		return entryTime;
+	}
+}
+
+
 float Entity::distanceBetweenTwoRects(const SDL_Rect &r1, const SDL_Rect &r2) {
 	SDL_Point e1, e2;
 	e1.x = r1.x + r1.w / 2;
@@ -203,6 +367,10 @@ float Entity::distanceBetweenTwoRects(const SDL_Rect &r1, const SDL_Rect &r2) {
 }
 float Entity::distanceBetweenTwoEntities(const Entity *e1, const Entity *e2) {
 	float d = abs(sqrt(pow(e2->x - e1->x, 2) + pow(e2->y - e1->y, 2)));
+	return d;
+}
+float Entity::distanceBetweenTwoPoints(float cx1, float cy1, float cx2, float cy2) {
+	float d = abs( sqrt( pow(cx2 - cx1, 2) +  pow(cy2 - cy1, 2) ));
 	return d;
 }
 float Entity::angleBetweenTwoEntities(const Entity *e1, const Entity *e2) {
